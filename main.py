021bf64e1a0567ae02e0792975f24a9fc46b80bf
Augustine Ehizuelen Abhulimen <76758281+Austinabhulimen@users.noreply.github.com>
2021-10-15 16:15:29 -0600

    first_version

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import time
+
+
+def timed_login():
+
+    def password():
+        passwords = []
+        x = 0
+        while x < 25:
+            values = ['(', '1', ')', 'r', '3', '4', '%', '&', '6', '*']
+
+            passwords.insert(x, (random.choice(values)))
+
+            x += 1
+
+        list1 = list(passwords[0:4])
+        print(list1)
+        return(list1)
+
+
+
+
+
+    def logic():
+        list1 = password()
+        startTime = time.time()
+        response = list(input("Please enter password: "))
+        executionTime = (time.time() - startTime)
+        print(executionTime)
+
+        if executionTime > 20:
+            print(f"Password expired!........{executionTime}")
+            time.sleep(5)
+            logic()
+        elif response == list1:
+            print("Welcome")
+
+        else: print("Try again, wrong password")
+
+    logic()
+
+
+timed_login()
+
+
