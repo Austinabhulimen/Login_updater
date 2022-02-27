66d05bebf06106976e155e66b7ad8e5c05e0fe92
Augustine Ehizuelen Abhulimen <76758281+Austinabhulimen@users.noreply.github.com>
2022-02-27 16:14:01 -0700

    Added the sending aparatus to the main logic

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
-import random
+import Password_generator
 import time
+from twilio.rest import Client
+def logic():
+    list1 = Password_generator.password()
+
+    # Your Account Sid and Auth Token from twilio.com / console
+    account_sid = 'ACf0169bad626e36d7404199ae55ecd308'
+    auth_token = '8d6f6248c77938b1e70b7fa098ed9a03'
+
+    client = Client(account_sid, auth_token)
+
+    ''' Change the value of 'from' with the number 
+    received from Twilio and the value of 'to'
+    with the number in which you want to send message.'''
+
+    stringed = "".join(list1)
+
+    messages = stringed
+
+    message = client.messages.create(
+        from_='+18593507135',
+        body=messages,
+        to='+13136526912'
+    )
 
 
-def timed_login():
 
-    def password():
-        passwords = []
-        x = 0
-        while x < 25:
-            values = ['(', '1', ')', 'r', '3', '4', '%', '&', '6', '*']
+    startTime = time.time()
+    response = list(input("Please enter password: "))
+    executionTime = (time.time() - startTime)
+    print(executionTime)
 
-            passwords.insert(x, (random.choice(values)))
+    if executionTime > 20:
+        print(f"Password expired!........{executionTime}")
+        time.sleep(5)
+        logic()
+    elif response == list1:
+        print("Welcome")
+        time.sleep(4)
+        quit()
 
-            x += 1
-
-        list1 = list(passwords[0:4])
-        print(list1)
-        return(list1)
-
-
-
-
-
-    def logic():
-        list1 = password()
-        startTime = time.time()
-        response = list(input("Please enter password: "))
-        executionTime = (time.time() - startTime)
-        print(executionTime)
-
-        if executionTime > 20:
-            print(f"Password expired!........{executionTime}")
-            time.sleep(5)
-            logic()
-        elif response == list1:
-            print("Welcome")
-
-        else: print("Try again, wrong password")
+    else:
+        print("Try again, wrong password")
 
     logic()
 
-
-timed_login()
+logic()
