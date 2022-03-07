ce152eb7efbad88d98b77b5c88e1c65dc9545e1a
Augustine Ehizuelen Abhulimen <76758281+Austinabhulimen@users.noreply.github.com>
2022-03-07 10:17:31 -0700

    Update Password_generator.py
    Update the password generator using OOP to give more structure to the code

--- Password_generator.py
+++ Password_generator.py
@@ -1,17 +1,19 @@
 import random
-def password():
-    passwords = []
-    x = 0
-    while x < 25:
-        values = ['(', '1', ')', 'r', '3', '4', '%', '&', '6', '*']
 
-        passwords.insert(x, (random.choice(values)))
-        x += 1
+class password_gen:
+    def __init__(self, values, passwords, list1):
+        self.values = values
+        self.passwords = passwords
+        self.list1 = list1
+    def password():
+        passwords = []
+        x = 0
+        while x < 25:
+            values = ['(', '1', ')', 'r', '3', '4', '%', '&', '6', '*']
+            passwords.insert(x, (random.choice(values)))
+            x += 1
+        list1 = list(passwords[0:4])
+        return(list1)
 
-    list1 = list(passwords[0:4])
 
-
-
-    return(list1)
-
-password()
+    password()
